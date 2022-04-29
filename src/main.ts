1bd63b1205b2a09477d94106a588d1b6bbb36581
stephaned <stephane.dupre@indigen.com>
2022-04-29 15:49:00 +0200

    init

---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import { createApp } from "vue";
+import { createRouter, createWebHistory } from "vue-router";
+import App from "./App.vue";
+
+import Home from "./views/Home.vue";
+import About from "./views/About.vue";
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: "/", name: "home", component: Home },
+    { path: "/about", name: "about", component: About },
+  ],
+});
+
+createApp(App).use(router).mount("#app");
---
+++ src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <p>{{ test }}</p>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  setup() {
+    const test = "test";
+
+    return { test };
+  },
+});
+</script>
